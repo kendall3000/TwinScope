23754e857963b40b058fd13ca3d4c0203a1e7730
Kendall <kendallramos03@gmail.com>
2024-09-03 21:01:24 -0500

    added some better css and organization to resume page

--- Kendall/assets/js/main.js
+++ Kendall/assets/js/main.js
@@ -19,3 +19,26 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const fadeEffect = document.querySelector('.fade-effect');
+
+//     const updateFadeEffect = () => {
+//         const scrollTop = window.pageYOffset;
+//         const windowHeight = window.innerHeight;
+//         const documentHeight = document.body.scrollHeight;
+
+//         // Calculate fade effect based on scroll position
+//         const fadeHeight = windowHeight * 0.1; // Adjust this value for the height of the fade effect
+//         const fadeStart = Math.max(scrollTop - fadeHeight, 0);
+//         const fadeEnd = Math.min(scrollTop + windowHeight + fadeHeight, documentHeight);
+
+//         fadeEffect.style.background = `
+//             linear-gradient(to bottom, 
+//             rgba(2, 33, 54, ${Math.min(scrollTop / fadeStart, 1)}) 0%, 
+//             rgba(2, 33, 54, ${Math.min((scrollTop + windowHeight) / fadeEnd, 1)}) 100%)`;
+//     };
+
+//     window.addEventListener('scroll', updateFadeEffect);
+//     window.addEventListener('resize', updateFadeEffect); // Update on resize to handle viewport changes
+// });
